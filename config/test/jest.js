817f1f5bb9d40951950f9eb92cf4f57e79463a33
= <machica@gmail.com>
2021-11-08 20:48:51 +0100

    Cypress folder ignored in jest tests

--- config/test/jest.js
+++ config/test/jest.js
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   restoreMocks: true,
   moduleDirectories: ['node_modules', 'src'],
+  testPathIgnorePatterns: ['node_modules', 'cypress'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/config/test/setup-after.ts'],
 };
